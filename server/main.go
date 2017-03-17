73015feaa55dcba9d9816b5ce36d6576eb28f86d
Alexey Andreev <bezumny.tochk@gmail.com>
2017-03-17 20:28:00 +0400

    Added httpd server and basic auth function

--- server/main.go
+++ server/main.go
@@ -2,9 +2,20 @@ package main
 
 import  (
 	"log"
+	"net/http"
 )
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	
+}
 
 func main() {
 
+
+	http.HandleFunc("/auth/", authHandler)
 	log.Println("Server started at port :80")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
