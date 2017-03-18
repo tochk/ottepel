b72ac93d6bd5b9caeb8bf2fc865725aaef216a35
Tuseeva Elena <etuseeva@griddynamics.com>
2017-03-18 08:33:34 +0300

    Add load photo

--- client/src/app/photos/photos.component.ts
+++ client/src/app/photos/photos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Photo} from "../classes/photo";
+import {ActivatedRoute, Params} from '@angular/router';
+import {RequestService} from "../services/request.service";
 
 @Component({
   selector: 'app-photos',
@@ -10,9 +12,20 @@ export class PhotosComponent implements OnInit {
 
   photos: Photo[];
 
-  constructor() { }
+  constructor(private route:ActivatedRoute,
+              private requestService: RequestService) { }
 
   ngOnInit() {
+    this.route.params.forEach((params:Params) => {
+      let convId = +params['convId'];
+      this.requestService.getPhotos(convId).subscribe((res) => {
+        this.photos = res;
+      });
+    });
+
+    // this.photos = [];
+    // for (let i = 0; i < 10; i++)
+    //   this.photos.push(new Photo("https://pp.userapi.com/c639825/v639825919/f168/LRPf8ZzAGCk.jpg"));
   }
 
 }
